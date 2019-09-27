a980be0445476a2bc93e5f12d14802ba05a0be2a
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-09-27 11:09:34 -0300

    Adiciona formatação do código com base no Prettier

--- src/components/TableContrato/Head.jsx
+++ src/components/TableContrato/Head.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Head = ({ listaContratos, nomeColunas }) => {
-    return (
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          {
-            listaContratos.map(key => <th key={key} scope="col">{nomeColunas[key] || key}</th>)
-          }
-        </tr>
-      </thead>
-    )
-  }
-  
-export default Head  
+  return (
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        {listaContratos.map(key => (
+          <th key={key} scope="col">
+            {nomeColunas[key] || key}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
+
+export default Head;
--- src/components/TableContrato/index.jsx
+++ src/components/TableContrato/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import Row from './Row'
-import Head from './Head'
-
+import Row from "./Row";
+import Head from "./Head";
 
 const Table = ({ dadosContrato, nomeColunas }) => {
-  const listaContratos = Object.keys(dadosContrato[0])
+  const listaContratos = Object.keys(dadosContrato[0]);
   return (
     <div class="table">
       <table class="table">
         <Head listaContratos={listaContratos} nomeColunas={nomeColunas} />
         <tbody>
-          { dadosContrato.map((registroTabela, key)=> <Row registroTabela={registroTabela} id={key} />) }
+          {dadosContrato.map((registroTabela, key) => (
+            <Row registroTabela={registroTabela} id={key} />
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
