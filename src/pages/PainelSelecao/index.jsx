9ecb56a8d370d2210b1bbde15295ff50e51accf7
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-10-04 15:43:40 -0300

    Aplica componente card equipamento na página de painel de seleção
    Permite a exibição dos cards tipo de equipamento na pagina de painel de
    seleção.

--- src/pages/PainelSelecao/index.jsx
+++ src/pages/PainelSelecao/index.jsx
@@ -4,11 +4,18 @@ import CardSection from "../../components/CardSection";
 import IconeEquipamento from "../../assets/images/icon-equipamento.svg";
 import IconeServico from "../../assets/images/icon-servicos.svg";
 import IconeMapa from "../../assets/images/icon-mapa.svg";
+import CardEquipamento from "../../components/CardEquipamento";
+
+const tipoEquipamento = {
+  educacionais: "Unidades Educacionais",
+  centros: "Centros Educacionais Unificados",
+  administrativas: "Unidades Administrativas"
+};
 
 export default () => (
   <Page titulo="Painel de seleção">
     <CardSection icone={IconeEquipamento} titulo="Escolha Equipamento">
-      {/* Importar cards de equipamento */}
+      <CardEquipamento equipamento={tipoEquipamento} />
     </CardSection>
     <CardSection icone={IconeServico} titulo="Escolha Serviço">
       {/* Importar cards de serviços */}
