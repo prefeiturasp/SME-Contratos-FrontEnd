ee6dfd6827a85d5a98e02f697e8fb06cd23a0d2b
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-10-16 16:46:36 -0300

    Aplica logica de busca de contratos pela API
    Implementa busca por todos os contratos e contratos de gestor.

--- src/pages/ListaContrato/index.jsx
+++ src/pages/ListaContrato/index.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import { TableContrato } from "../../components/TableContrato";
+import { getMeusContratos, getContratos } from "../../service/ContratoService.js";
+import { Button } from "primereact/button";
 import Page from "../../components/Page";
-import TableContrato from "../../components/TableContrato";
-import dadosContrato from "./dadosContrato.json";
-import "./style.scss";
+import Container from "../../components/Container";
 
-const nomeColunas = {
-  termoContrato: "Termo de Contrato",
-  sitContrato: "Sit. Contrato",
-  empresa: "Empresa",
-  vigencia: "Vigência",
-  data: "Data"
-};
+export class ListaContrato extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contratos: []
+    };
+  }
 
-export default () => (
-  <Page titulo="Lista de Contratos">
-    <div className="bg-table">
-      <div className="table">
-        <TableContrato
-          dadosContrato={dadosContrato}
-          nomeColunas={nomeColunas}
-        />
-      </div>
-    </div>
-  </Page>
-);
+  setaTodosContratos() {
+    getContratos().then(contratos => {
+      console.log(contratos);
+      this.setState({ contratos });
+    });
+  }
+
+  setaMeusContratos() {
+    getMeusContratos().then(contratos => {
+      // console.log(contratos)
+      this.setState({ contratos });
+    });
+  }
+
+  componentDidMount() {
+    this.setaMeusContratos();
+  }
+
+  render() {
+    const { contratos } = this.state;
+    return (
+      <Page>
+        <Container>
+          <h1>Lista de Contratos</h1>
+          <div style={{ paddingBottom: "10px" }}>
+            <Button
+              label="Meus Contratos"
+              style={{ marginRight: "10px" }}
+              onClick={this.setaMeusContratos.bind(this)}
+            />
+            <Button
+              label="Todos Contratos"
+              onClick={this.setaTodosContratos.bind(this)}
+            />
+          </div>
+          <TableContrato contratos={contratos} />
+        </Container>
+      </Page>
+    );
+  }
+}
+export default ListaContrato;
