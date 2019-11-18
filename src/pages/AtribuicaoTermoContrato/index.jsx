3ea1a01882ca51fce031e0ee038cbbc1ea60ff32
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-11-18 16:26:36 -0300

    Adiciona mensagem de sucesso depois de editar

--- src/pages/AtribuicaoTermoContrato/index.jsx
+++ src/pages/AtribuicaoTermoContrato/index.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import { Messages } from "primereact/messages";
 import Page from "../../components/Global/Page";
 import Container from "../../components/Global/Container";
 import AtribuirTermoContrato from "../../components/Contratos/AtribuirTermoContrato";
 import ListarTermoContrato from "../../components/Contratos/ListarTermoContrato";
 
 export default class AtribuicaoTermoContrato extends Component {
+  constructor() {
+    super();
+    this.mensagemSucessoEdicao = this.mensagemSucessoEdicao.bind(this);
+  }
+
+  mensagemSucessoEdicao() {
+    window.scrollTo(0, 0);
+    this.messages.show({
+      severity: "success",
+      life: 5000,
+      detail: "Alterações realizadas com sucesso"
+    });
+  }
+
   render() {
     return (
-      <Page titulo="Atribuir Termo de Contrato">
+      <Page>
+        <Messages ref={el => (this.messages = el)}></Messages>
+        <h4>Atribuir Termo de Contrato</h4>
         <Container>
           <Container
             subtitulo="Atribuir Termo de Contrato"
@@ -16,7 +33,9 @@ export default class AtribuicaoTermoContrato extends Component {
             <AtribuirTermoContrato />
           </Container>
           <Container>
-            <ListarTermoContrato />
+            <ListarTermoContrato
+              mensagemSucessoEdicao={this.mensagemSucessoEdicao}
+            />
           </Container>
         </Container>
       </Page>
