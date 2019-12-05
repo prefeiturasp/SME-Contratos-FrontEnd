aed0e8326be7e417b75a21ff94b14a0568c82b4d
wesley1263 <weslei.souza@amcom.com.br>
2019-12-05 17:46:45 -0300

    Adiciona novos botões de modelo de ateste
     - Adiciona botõ voltar
    - Adiciona botão salvar
    - Adiciona botão excluir grupo

--- src/pages/ModeloAteste/Modelo.jsx
+++ src/pages/ModeloAteste/Modelo.jsx
@@ -1,25 +1,37 @@
 import React, { useState, useEffect, Fragment, useCallback } from "react";
-import { FormGroup, Input, Label, Card } from "reactstrap";
+import {
+  FormGroup,
+  Input,
+  Label,
+  Card,
+  Button as ButtonBootstrap
+} from "reactstrap";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import Grupo from "./Grupo";
-import { Button as AntButton } from "antd";
-import { criaModeloAteste } from "../../service/ModeloAteste.service";
+import { Button as AntButton, Switch } from "antd";
+import {
+  criaModeloAteste,
+  alteraModeloAteste
+} from "../../service/ModeloAteste.service";
 import { CREATED } from "http-status-codes";
 import { redirect } from "../../utils/redirect";
 import { setFlashMessage } from "../../utils/flashMessages";
 import { getUrlParams } from "../../utils/params";
+import { Row, Col } from "reactstrap";
 
 const Modelo = props => {
+  const [visivel, setVisivel] = useState(false);
   const [modelo, setModelo] = useState({});
   const [modoVisualizacao, setModoVisualizacao] = useState(true);
-  const [visivel, setVisivel] = useState(false);
+  const [incluir, setIncluir] = useState(true);
 
   useEffect(() => {
-    setModelo(props.modelo); 
+    setModelo(props.modelo);
     const parametro = getUrlParams();
-    if(!parametro.uuid){
-      setModoVisualizacao(false)
+    if (!parametro.uuid) {
+      setIncluir(false);
+      setModoVisualizacao(false);
     }
   }, [props.modelo]);
 
@@ -51,16 +63,21 @@ const Modelo = props => {
     setModelo({ ...modelo });
   };
 
-  const ativaModoEdicao = () => {
-    setModoVisualizacao(false);
+  const excluirGrupo = index => {
+    modelo.grupos_de_verificacao.splice(index, 1);
+    setModelo({ ...modelo });
   };
 
   const confirmaModelo = async () => {
     const resultado = await criaModeloAteste(modelo);
     if (resultado.status === CREATED) {
-      setFlashMessage("Modelo de ateste criado com sucesso","sucesso")
-      redirect("#/listar-modelos-ateste")
+      setFlashMessage("Modelo de ateste criado com sucesso", "sucesso");
+      redirect("#/listar-modelos-ateste");
     }
+  };
+
+  const alteraModelo = async () => {
+    const resultado = await alteraModeloAteste(modelo);
   };
 
   const mostraAlertaContainer = useCallback(event => {
@@ -71,9 +88,23 @@ const Modelo = props => {
     modoVisualizacao === false && modelo.titulo && modelo.grupos_de_verificacao
       ? false
       : true;
-
+  const mensagemConfirmacao = incluir ? 'Deseja confirmar criação de novo modelo de ateste?' : 'Deseja confirmar alteração deste modelo de ateste?'
   return (
     <Fragment>
+      <FormGroup className="d-flex flex-row-reverse mt-3">
+        <Button
+          disabled={habilitaBotao}
+          className="btn-coad-primary"
+          label="Salvar"
+          onClick={exibeDialog}
+        />
+        <ButtonBootstrap
+          onClick={() => redirect("#/listar-modelos-ateste")}
+          className="btn-coad-blue mx-2"
+        >
+          <i className="fas fa-arrow-left" /> Voltar
+        </ButtonBootstrap>
+      </FormGroup>
       <Dialog
         header={"Confirmar"}
         visible={visivel}
@@ -81,22 +112,46 @@ const Modelo = props => {
         modal={true}
         onHide={fechaDialog}
       >
-        <span>Deseja confirmar criação de novo modelo de ateste?</span>
+<span>{mensagemConfirmacao}</span>
         <FormGroup className="pt-4 d-flex justify-content-end">
           <Button
             className="btn-coad-background-outline mx-2"
             onClick={fechaDialog}
             label="Cancelar"
           />
-
-          <Button
-            disabled={habilitaBotao}
-            className="btn-coad-primary"
-            label="Confirmar"
-            onClick={confirmaModelo}
-          />
+          {!incluir ? (
+            <Button
+              disabled={habilitaBotao}
+              className="btn-coad-primary"
+              label="Confirmar"
+              onClick={confirmaModelo}
+            />
+          ) : (
+            <Button
+              disabled={habilitaBotao}
+              className="btn-coad-primary"
+              label="Alterar"
+              onClick={alteraModelo}
+            />
+          )}
         </FormGroup>
       </Dialog>
+      <Row>
+        <Col lg={6} xl={6}>
+          <h6>
+            <i className="fas fa-sm fa-file-signature" /> Modelo de Ateste
+          </h6>
+        </Col>
+        <Col className="d-flex justify-content-end">
+          <Label className="px-3">Modo de edição</Label>
+          <Switch
+            // size="small"
+            defaultChecked={!modoVisualizacao}
+            onChange={() => setModoVisualizacao(!modoVisualizacao)}
+          />
+        </Col>
+      </Row>
+      <br />
       <FormGroup>
         <Label className="font-weight-bold">Título do modelo de ateste</Label>
         <Input
@@ -119,6 +174,16 @@ const Modelo = props => {
                 mostraAlerta={mostraAlertaContainer}
                 modoVisualizacao={modoVisualizacao}
               />
+              <Row>
+                <Col className="d-flex justify-content-end">
+                  <Button
+                    disabled={modoVisualizacao}
+                    className="btn-coad-background-outline"
+                    label="Excluir grupo"
+                    onClick={() => excluirGrupo(i)}
+                  />
+                </Col>
+              </Row>
             </Card>
           ))
         ) : (
@@ -147,10 +212,15 @@ const Modelo = props => {
         <Button
           disabled={habilitaBotao}
           className="btn-coad-primary"
-          label="Confirmar"
+          label="Salvar"
           onClick={exibeDialog}
         />
-        <Button className="btn-coad-background-outline mx-2" label="Cancelar" />
+        <ButtonBootstrap
+          onClick={() => redirect("#/listar-modelos-ateste")}
+          className="btn-coad-blue mx-2"
+        >
+          <i className="fas fa-arrow-left" /> Voltar
+        </ButtonBootstrap>
       </FormGroup>
     </Fragment>
   );
