5e7d5d4d15190b01d17de14d03d3ba64efd409cd
Luis <lhzimmermann@gmailc.om>
2022-04-04 16:04:33 -0300

    Melhoria nas mensagems de feedback (Componente de toast)

---
+++ src/contexts/ToastContext.js
@@ -0,0 +1,64 @@
+import React, { useRef, createContext } from "react";
+import { Toast } from "primereact/toast";
+
+export const ToastContext = createContext({
+  showSuccess() {},
+  showInfo() {},
+  showWarn() {},
+  showError() {},
+});
+
+export const ToastContextProvider = ({ children }) => {
+  const toast = useRef(null);
+
+  const showSuccess = (mensagem, titulo = "Sucesso") => {
+    toast.current.show({
+      severity: "success",
+      summary: titulo,
+      detail: mensagem,
+      life: 7000,
+      sticky: true,
+    });
+  };
+
+  const showInfo = (mensagem, titulo = "Informação") => {
+    toast.current.show({
+      severity: "info",
+      summary: titulo,
+      detail: mensagem,
+      life: 7000,
+    });
+  };
+
+  const showWarn = (mensagem, titulo = "Aviso") => {
+    toast.current.show({
+      severity: "warn",
+      summary: titulo,
+      detail: mensagem,
+      life: 7000,
+    });
+  };
+
+  const showError = (mensagem, titulo = "Erro") => {
+    toast.current.show({
+      severity: "error",
+      summary: titulo,
+      detail: mensagem,
+      life: 10000,
+    });
+  };
+
+  return (
+    <ToastContext.Provider
+      value={{
+        showSuccess,
+        showInfo,
+        showWarn,
+        showError,
+      }}
+    >
+      {children}
+      <Toast ref={toast} />
+    </ToastContext.Provider>
+  );
+};
