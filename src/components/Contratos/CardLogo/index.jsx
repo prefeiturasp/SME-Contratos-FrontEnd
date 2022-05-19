1d4c10cb980930ddd4e096678d4a2a78b5e583fb
Diogo Santos <euamofisica2006@hotmail.com>
2022-05-19 10:56:16 -0300

    correção eslint

--- src/components/Contratos/CardLogo/index.jsx
+++ src/components/Contratos/CardLogo/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardImg, Col } from "reactstrap";
+import { Card, CardBody, CardTitle, Col } from "reactstrap";
 import "./style.scss";
 import { redirect } from "../../../utils/redirect";
 
