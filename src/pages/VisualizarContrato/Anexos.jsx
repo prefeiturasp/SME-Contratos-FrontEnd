e9ae357d0a22f190bcbaea9e22e112ebfedbfd5f
Diogo Santos <euamofisica2006@hotmail.com>
2022-07-13 08:49:50 -0300

    remove tipo de documentos de anexo para ficar apenas anexo

--- src/pages/VisualizarContrato/Anexos.jsx
+++ src/pages/VisualizarContrato/Anexos.jsx
@@ -1,73 +1,38 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
-import { CoadTabs } from "../../components/Contratos/CoadTabs";
 import * as CONFIG from "../../configs/config.constants";
 import "react-dropzone-uploader/dist/styles.css";
 import Dropzone from "react-dropzone-uploader";
-import "./style.scss";
 import { getUrlParams } from "../../utils/params";
 import { getDocumentosFiscaisByContrato } from "../../service/DocumentosContrato.service";
+import "./style.scss";
 
 // https://github.com/fortana-co/react-dropzone-uploader
 class Anexos extends Component {
   state = {
     url: `${CONFIG.API_URL}/documentos-fiscais/`,
-    docsDres: [],
-    docsUnidades: [],
-    docsOutros: [],
+    anexos: [],
     uuidContrato: null,
   };
 
   carregaAnexosContrato = async () => {
-    const { docsDres, docsUnidades, docsOutros } = this.state;
+    const { anexos } = this.state;
     const param = getUrlParams();
     this.setState({ uuidContrato: param.uuid });
     const docs = await getDocumentosFiscaisByContrato(param.uuid);
     docs.forEach(value => {
-      if (value.tipo_unidade === "FISCAL_DRE") {
-        const fileName = value.anexo.split("/").slice(-1)[0];
-        docsDres.push({ href: value.anexo, name: fileName });
-        this.setState({ docsDres });
-      }
-      if (value.tipo_unidade === "FISCAL_UNIDADE") {
-        const fileName = value.anexo.split("/").slice(-1)[0];
-        docsUnidades.push({ href: value.anexo, name: fileName });
-        this.setState({ docsUnidades });
-      }
-      if (value.tipo_unidade === "FISCAL_OUTROS") {
-        const fileName = value.anexo.split("/").slice(-1)[0];
-        docsOutros.push({ href: value.anexo, name: fileName });
-        this.setState({ docsOutros });
-      }
+      const fileName = value.anexo.split("/").slice(-1)[0];
+      anexos.push({ href: value.anexo, name: fileName });
+      this.setState({ anexos });
     });
   };
 
-  getUploadParamsDre = ({ file }) => {
+  getUploadParams = ({ file }) => {
     const { uuidContrato, url } = this.state;
     const fields = {
       anexo: file,
       contrato: uuidContrato,
-      tipo_unidade: "FISCAL_DRE",
-    };
-    return { fields, url: url };
-  };
-
-  getUploadParamsUnidade = ({ file }) => {
-    const { uuidContrato, url } = this.state;
-    const fields = {
-      anexo: file,
-      contrato: uuidContrato,
-      tipo_unidade: "FISCAL_UNIDADE",
-    };
-    return { fields, url: url };
-  };
-
-  getUploadParamsOutros = ({ file }) => {
-    const { uuidContrato, url } = this.state;
-    const fields = {
-      anexo: file,
-      contrato: uuidContrato,
-      tipo_unidade: "FISCAL_OUTROS",
+      tipo_unidade: "FISCAL_DRE", //RETIRAR
     };
     return { fields, url: url };
   };
@@ -78,118 +43,48 @@ class Anexos extends Component {
 
   render() {
     const { disabilitado } = this.props;
-    const { docsDres, docsUnidades, docsOutros } = this.state;
+    const { anexos } = this.state;
     return (
       <div className="coad-anexos">
-        <CoadTabs
-          titulo1={"Carregar documentos de Fiscais"}
-          titulo2={"Carregar outros documentos"}
-          conteudo1={
-            <div>
-              <Row>
-                <Col className="pb-5">
-                  <label className="font-weight-bold">
-                    Anexar documentos Fiscal DRE
-                  </label>
-                  <Dropzone
-                    getUploadParams={this.getUploadParamsDre}
-                    // onChangeStatus={handleChangeStatus}
-                    inputContent="Clique ou arraste arquivos nesta área para upload"
-                    inputWithFilesContent="Adicionar mais arquivos"
-                    disabled={disabilitado}
-                  />
-                  <Row className="mt-2">
-                    {docsDres
-                      ? docsDres.map((file, i) => {
-                          return (
-                            <Col xl={12} lg={12} key={i}>
-                              <a
-                                className="text-danger"
-                                href={file.href}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                key={i}
-                              >
-                                <i className="fas fa-xl fa-paperclip"></i>{" "}
-                                {file.name}
-                              </a>
-                            </Col>
-                          );
-                        })
-                      : ""}
-                  </Row>
-                </Col>
-              </Row>
-
-              <Row>
-                <Col className="pb-5">
-                  <label className="font-weight-bold">
-                    Anexar documentos Fiscal Unidade
-                  </label>
-                  <Dropzone
-                    getUploadParams={this.getUploadParamsUnidade}
-                    inputContent="Clique ou arraste arquivos nesta área para upload"
-                    inputWithFilesContent="Adicionar mais arquivos"
-                    disabled={disabilitado}
-                  />
-                  <Row className="mt-2">
-                    {docsUnidades
-                      ? docsUnidades.map((file, i) => {
-                          return (
-                            <Col xl={12} lg={12} key={i}>
-                              <a
-                                className="text-danger"
-                                href={file.href}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                              >
-                                <i className="fas fa-xl fa-paperclip"></i>{" "}
-                                {file.name}
-                              </a>
-                            </Col>
-                          );
-                        })
-                      : ""}
-                  </Row>
-                </Col>
-              </Row>
-            </div>
-          }
-          conteudo2={
-            <Row>
-              <Col className="pb-5">
-                <label className="font-weight-bold">
-                  Anexar outros documentos
-                </label>
-                <Dropzone
-                  getUploadParams={this.getUploadParamsOutros}
-                  inputContent="Clique ou arraste arquivos nesta área para upload"
-                  inputWithFilesContent="Adicionar mais arquivos"
-                  disabled={disabilitado}
-                />
-                <Row className="mt-2">
-                  {docsOutros
-                    ? docsOutros.map((file, i) => {
-                        return (
-                          <Col xl={12} lg={12} key={i}>
-                            <a
-                              className="text-danger"
-                              href={file.href}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <i className="fas fa-xl fa-paperclip"></i>{" "}
-                              {file.name}
-                            </a>
-                          </Col>
-                        );
-                      })
-                    : ""}
-                </Row>
-              </Col>
+        <Row>
+          <Col className="pb-5">
+            <Row className="row-anexo">
+              <div className="title-anexo">Anexar documentos</div>
+              <div className="aviso-anexo">
+                IMPORTANTE: anexe documentos nos formatos .pdf, .doc, .docx,
+                .xls. xlsx, .png, .jpg e .jpeg.
+              </div>
             </Row>
-          }
-        />
+            <Dropzone
+              getUploadParams={this.getUploadParams}
+              // onChangeStatus={handleChangeStatus}
+              inputContent="Clique ou arraste arquivos nesta área para upload"
+              inputWithFilesContent="Adicionar mais arquivos"
+              maxSizeBytes={1024 * 1024 * 15}
+              disabled={disabilitado}
+            />
+
+            <Row className="mt-2">
+              {anexos
+                ? anexos.map((file, i) => {
+                    return (
+                      <Col xl={12} lg={12} key={i}>
+                        <a
+                          className="text-danger"
+                          href={file.href}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          key={i}
+                        >
+                          <i className="fas fa-xl fa-paperclip"></i> {file.name}
+                        </a>
+                      </Col>
+                    );
+                  })
+                : ""}
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
