6f77295a19d7897f519fd994cc9af5ec8a4ef668
Diogo Santos <euamofisica2006@hotmail.com>
2022-09-15 15:02:18 -0300

    ajuste mascara dinamica do telefone

--- src/pages/Empresas/helper.js
+++ src/pages/Empresas/helper.js
@@ -4,8 +4,10 @@ export const mascaraTelefoneOuCelular = telefone => {
     .replace("-", "")
     .replace("_", "")
     .replace(" ", "");
-  const ehCelular = fone.length === 11;
-
+  const ehCelular = fone.length > 10;
+  if (fone > 11) {
+    fone = fone.substr(0, 11);
+  }
   if (ehCelular) return fone.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
   else return fone.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
 };
